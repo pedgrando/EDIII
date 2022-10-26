60384f93317591c9583456586ff0daee5dd5f99f
amigo <pedrofrancoso@usp.br>
2022-10-26 14:24:34 -0300

    teste de commit

--- EX2/funcoes.c
+++ EX2/funcoes.c
@@ -185,6 +185,7 @@ int LeRegistro(FILE *file_in, Registro *Register){
         if (aux == ',') break;
         Register->siglaPais[i] = aux;
         i++;
+		/teste em
     }
     if (i > 0) {
         Register->siglaPais[i] = '\0';
