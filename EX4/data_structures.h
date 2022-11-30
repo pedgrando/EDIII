f20862660a3ae8686cf99389c2baa0b9e7f16dfc
amigo <pedrofrancoso@usp.br>
2022-11-30 19:20:56 -0300

    grafos

---
+++ EX4/data_structures.h
@@ -0,0 +1,66 @@
+#ifndef __DATA_STRUCTURES_H__
+#define __DATA_STRUCTURES_H__
+
+#include <stdio.h>
+#include<stdlib.h>
+
+// CONSTANTES IMPORTANTES
+
+#define PAG_DISCO 960
+#define CAB 21
+#define DEL '|'
+#define LIXO '$'
+#define REM '*'
+#define TAM_NO 65
+
+// CODIGO HASH DOS CAMPOS
+
+#define IDCONECTA 906
+#define SIGLAPAIS 925
+#define IDPOPSCONECTADO 1503
+#define UNIDADEMEDIDA 1310
+#define VELOCIDADE 1040
+#define NOMEPOPS 817
+#define NOMEPAIS 828
+
+// CONSTANTES PARA SELECAO DO TIPO DE BUSCA
+
+#define CONSULTA 1
+#define REMOCAO 0
+
+
+//STRUCTS DOS DADOS
+
+typedef struct cabecalho{
+	char status;
+	int topo;
+	int proxRRN;
+    int nroRegRem;
+    int nroPagDisco;  	//pagina tem 960 bytes
+    int qttCompacta; 
+} Cabecalho;
+
+typedef struct registro{
+    int campoVazio[7];
+    char removido;
+    int encadeamento;
+    int idConecta; 	//sem valores repetidos
+    char siglaPais[2];
+    int idPoPsConectado;
+    char unidadeMedida;
+    int velocidade;
+    char nomePoPs[30];
+    char nomePais[30];
+} Registro;
+
+
+typedef struct grafo{
+    int numVertices;
+    int grauMax;
+    int** arestas;
+    float** pesos;
+    int* grau;
+} Grafo;
+
+
+#endif
---
+++ EX4/funcionalidades.h
@@ -0,0 +1,19 @@
+#ifndef __FUNCIONALIDADES_H__
+#define __FUNCIONALIDADES_H__
+
+#include <stdio.h>
+#include<stdlib.h>
+#include "data_structures.h"
+#include "miscelaneous.h"
+
+//FUNCIONALIDADES
+
+void funcionalidade11(FILE *file);
+
+void funcionalidade12(FILE *file);
+
+void funcionalidade13(FILE *file);
+
+void funcionalidade14(FILE *file);
+
+#endif
