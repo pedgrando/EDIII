2c65ab6a6c693022e123ff580e2ec4e5d8e6c77e
pedgrando <pedroantoniobg@hotmail.com>
2022-12-18 12:28:35 -0300

    automatic make commit

--- EX4/funcoes_principais.c
+++ EX4/funcoes_principais.c
@@ -7,32 +7,50 @@
 #include "miscelaneous.h"
 #include "funcoes_principais.h"
 
+// Algoritmo de Dijkstra -> encontra o menor caminho possivel em um grafo, partindo de um vertice ate outro
+//
+// O algoritmo de Dijkstra basicamente mede as distancias relativas de todos os vertices do grafo em relacao ao vertice de origem
+// Partindo do vertice de origem, o algoritmo caminha pelos vertices vizinhos atualizando a distancia entre eles para a menor possivel e o antecessor desse vertice para aquele que gera o vertice que gera esse menor caminho
+// No processo, existe a verificacao constante se foi encontrado um novo caminho menor ate certo vertice
+//
+// Ao fim do algoritmo, eh possivel saber a distancia relativa entre o vertice de origem e o fim, e o caminho que gera essa menor distancia
+//
 
 void Dijkstra(Grafo *grafo, int origem, int *distancia, int *predecessor, int num_vertices){
 	int i, *pos_heap = malloc(num_vertices*sizeof(int)), dist, id;
 	
+	// pos_heap eh um vetor que mantem registrado onde cada vertice esta na heap (v[x] eh a posicao na heap do vertice de idConecta x+1)
+
 	dado aux;	
 
-
+	// inicializa todas as distancias relativas como infinitas e os predecessores do vertices como invalidos
 	for(i = 0; i < num_vertices; i++){
 		distancia[i] = INT_MAX;
 		predecessor[i] = -1;
 	}
+
+	// define a distancia relativa entre a origem pra ela mesma como zero
 	distancia[origem - 1] = 0;
 
+	// cria-se uma heap que sera utilizada como lista de prioridades
+	// inicializa-se a heap colocando todos os vertices 
 	Heap *h = criaHeap(num_vertices);
-	for(int i = 0; i < num_vertices; i++){
+	for(i = 0; i < num_vertices; i++){
 		h->dados[i].idConecta = i+1;
 		h->dados[i].velocidade = distancia[i];
 		pos_heap[i] = i;
 	}
 
+	// atualiza o tamanho da heap
 	h->tam_heap = num_vertices;
-
+	
+	// coloca-se o vertice de origem como primeiro da lista de prioridades, mantendo-na ordenada	
 	troca(&h->dados[origem - 1], &h->dados[0]);
 	trocaInt(&pos_heap[origem - 1], &pos_heap[0]);
 
+	// enquanto nao forem percorridos todos os nos
 	while(h->tam_heap > 0){
+		// pega o vertice para ser analisado na lista de prioridades
 		removeMin(h, pos_heap, &aux);	
 
 		int vertice = aux.idConecta;
@@ -40,16 +58,26 @@ void Dijkstra(Grafo *grafo, int origem, int *distancia, int *predecessor, int nu
 		// varre-se a lista de adjacencia do vertice atual e faz-se o relaxamento das arestas em relacao a ele	
 		no *atual = *(grafo[vertice - 1].listaAdj);
 		while(atual != NULL){
+			// variavel dist guarda a velocidade da aresta entre o vertice analisado atualmente e o vertice de onde esse parte
 			id = atual->idPoPs;
 			dist = atual->velocidade;
+
+			// se menor distancia conhecida para id > distancia vertice que leva a id em relacao a origem + distancia do vertice que leva a id ate id
+			// em outras palavras, se a distancia conhecida para um vertice eh maior que uma nova distancia obtida pelo caminho analisado
+			// caso isso ocorra, significa que foi encontrado outro caminho ate o vertice atual que passa pelo vertice retirado da heap
 			if(distancia[id - 1] > distancia[vertice - 1] + dist){
+				// atualiza as distancias e o predecessor (aquele que gera o caminho com essa distancia)
 				distancia[id - 1] = distancia[vertice - 1] + dist;
 				predecessor[id - 1] = vertice - 1;
 
 				aux.idConecta = id;
 				aux.velocidade = dist;
+
+				// insere o vertice analisado na heap para que se analise os proximos caminhos a partir desse vertice, que gerou um novo caminho melhor
 				atualizaHeap(h, pos_heap, aux);
 			}
+
+			// varre o proximo vertice da lista
 			atual = atual->prox;
 		}
 		
@@ -58,13 +86,26 @@ void Dijkstra(Grafo *grafo, int origem, int *distancia, int *predecessor, int nu
 	free(h);
 }
 
+// Busca em profundidad -> varre todo o grafo, classificando as arestas por cor
+// 
+// Nesse caso, como a busca em profundidade so eh utilizada para encontrar o numero de ciclos, nao eh necessario
+// classificar arestas como de cruzamento ou avanco, mas apenas saber que elas sao arestas dessa natureza, pois
+// conta-las resulta no numero de ciclos simples do grafo
+
 void dfs(Grafo *grafo, int num_vertices, int *cor){
 	int ciclos = 0;
 
+	// cores
+	// 0 -> branco
+	// 1 -> cinza
+	// 2 -> preto
+
+	// define todos os vertices como brancos, i.e., nao analisados ainda
 	for(int i = 0; i < num_vertices; i++){
 		cor[i] = 0;
 	}
 
+	// para cada vertice do grafo que nao foi analisado ainda, faz-se a analise de todos os vertices aos quais ele se liga
 	for(int i = 0; i < num_vertices; i++){
 		if(cor[i] == 0) dfs_loop(grafo, &ciclos, i, cor);	
 	}
@@ -73,21 +114,32 @@ void dfs(Grafo *grafo, int num_vertices, int *cor){
 }
 
 void dfs_loop(Grafo *grafo, int *ciclos, int x, int *cor){
+	// define-se a cor do vertice como cinza, i.e., em analise
 	cor[x] = 1;	
 
 	no *aux = *grafo[x].listaAdj;
 	
+	// varre-se a lista de vertices adjacentes analisando quem ainda nao foi analisado
+	// o procedimento da busca eh ir cada vez mais fundo, entao, logo ao se encontrar um vertice adjacente ao atual e que nao foi analisado
+	// aplica-se o mesmo procedimento para esse novo vertice
 	while(aux != NULL){
 		if(cor[aux->idPoPs - 1] == 0){
-	  		dfs_loop(grafo, ciclos, aux->idPoPs - 1, cor);	
-		} else if(cor[aux->idPoPs - 1] == 2){
+
+	  		dfs_loop(grafo, ciclos, aux->idPoPs - 1, cor);	// progride-se cada vez mais fundo no grafo
+									//
+		} else if(cor[aux->idPoPs - 1] == 2){ // caso o vertice analisado esteja se ligando a um vertice que ja foi analisado (preto, cor == 2), incrementa-se o numero de ciclos
+
 			(*ciclos)++;
+
 		}
 		aux = aux->prox;	
 	}
-	
+
+	// ao terminar de analisar toda a subarvore derivada do no atual, marca-o como fechado (cor == 2, preto)	
 	cor[x] = 2;
 }
+
+// Algoritmo de Edmond-Karp -> busca o fluxo maximo entre dois vertices do grafo
 
 void edmondkarp(Grafo *grafo, int num_vertices, int origem, int destino){
 	int **fluxo = malloc(sizeof(int *) * num_vertices);	
@@ -187,35 +239,5 @@ void edmondkarp(Grafo *grafo, int num_vertices, int origem, int destino){
 	free(capacidade);
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
